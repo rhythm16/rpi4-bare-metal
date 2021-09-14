e0667fc2ef610339e40697ef1739e767488c4dbb
Wei-Lin Chang <r09922117@csie.ntu.edu.tw>
2021-09-14 18:02:54 +0800

    change syscall interface and user stuff

--- include/sys.h
+++ include/sys.h
@@ -1,25 +1,22 @@
 #ifndef SYS_H
 #define SYS_H
 
-#define NR_SYSCALLS  4
+#define NR_SYSCALLS  3
 
 #define SYS_WRITE_NUM          0
-#define SYS_GET_FREE_PAGE_NUM  1
-#define SYS_CLONE_NUM          2
-#define SYS_EXIT_NUM           3
+#define SYS_FORK_NUM           1
+#define SYS_EXIT_NUM           2
 
 #ifndef __ASSEMBLER__
 
 #include "types.h"
 
 void sys_write(char * buf);
-int sys_clone(u64 stack);
-u64 sys_get_free_page();
+int sys_fork();
 void sys_exit();
 
 void call_sys_write(char * buf);
-int call_sys_clone(u64 fn, u64 arg, u64 stack);
-u64 call_sys_get_free_page();
+int call_sys_fork();
 void call_sys_exit();
 
 void sys_call_table_relocate();
--- user_space/init.c
+++ user_space/init.c
@@ -3,38 +3,31 @@
 #include "user_space/init.h"
 #include "utils.h"
 
+void loop(char *str)
+{
+    while (1) {
+        call_sys_write(str);
+        int i = 1000000;
+        while (i > 0) {
+            i--;
+        }
+    }
+}
+
 void user_process()
 {
     call_sys_write("pid 1 in user space\n");
-    u64 user_stack_page = call_sys_get_free_page();
-    if (user_stack_page < 0) {
-        call_sys_write("Error while allocating stack page for pid 2\n");
-        goto exit;
+    int pid = call_sys_fork();
+    if (pid < 0) {
+        call_sys_write("fork error\n");
+        call_sys_exit();
+        return;
     }
-    int err = call_sys_clone((u64)user_process1, (u64)"pid2\n", user_stack_page);
-    if (err < 0) {
-        call_sys_write("Error cloning pid 2\n");
-        goto exit;
+    if (pid == 0) {
+        loop("child");
     }
-    user_stack_page = call_sys_get_free_page();
-    if (user_stack_page < 0) {
-        call_sys_write("Error while allocating stack page for pid 3\n");
-        goto exit;
-    }
-    err = call_sys_clone((u64)user_process1, (u64)"pid3\n", user_stack_page);
-    if (err < 0) {
-        call_sys_write("Error cloning pid 3\n");
-        goto exit;
-    }
-exit:
-    call_sys_exit();
-}
-
-void user_process1(char *array)
-{
-    while (1) {
-        call_sys_write(array);
-        delay(1000000);
+    else {
+        loop("parent");
     }
 }
 
